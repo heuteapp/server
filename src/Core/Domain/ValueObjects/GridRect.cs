bb0c75c68e05e02d8c5590fe3bf9e185eb32d317
k4yr2 <kyrzkaya@gmail.com>
2026-02-24 14:59:56 +0300

    Add GridRect record struct with overlap detection method

---
+++ src/Core/Domain/ValueObjects/GridRect.cs
@@ -0,0 +1,17 @@
+namespace HeuteApp.Core.Domain;
+
+public readonly record struct GridRect(
+    int Row,
+    int Col,
+    int RowSpan,
+    int ColSpan)
+{
+    public bool Overlaps(GridRect other)
+    {
+        return
+            Col < other.Col + other.ColSpan &&
+            Col + ColSpan > other.Col &&
+            Row < other.Row + other.RowSpan &&
+            Row + RowSpan > other.Row;
+    }
+}
