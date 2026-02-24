042c7deda0bfdae12b7451676fae67e76f61415f
k4yr2 <kyrzkaya@gmail.com>
2026-02-24 18:06:42 +0300

    Add layout parameter to HeuteBoard class and implement layout management methods

--- src/Core/Domain/HeuteBoard.cs
+++ src/Core/Domain/HeuteBoard.cs
@@ -1,8 +1,23 @@
 namespace HeuteApp.Core.Domain;
 
-public class HeuteBoard(Guid id, Guid ownerId)
+public class HeuteBoard(Guid id, Guid ownerId, HeuteLayoutSnapshot layout)
 {
+    private HeuteLayoutSnapshot m_layout = layout;
+
+    //
+
     public Guid Id => id;
     
     public Guid OwnerId => ownerId;
+
+    public HeuteLayoutSnapshot Layout => m_layout;
+
+    //
+
+    public void ChangeLayout(HeuteLayoutSnapshot layout)
+    {
+        ArgumentNullException.ThrowIfNull(layout);
+
+        m_layout = layout;
+    }
 }
