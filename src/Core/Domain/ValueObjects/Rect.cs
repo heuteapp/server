632813965ec5062701aafb7fe2eb49830a7c3ad0
k4yr2 <kyrzkaya@gmail.com>
2026-02-24 15:13:03 +0300

    Add Rect record struct to represent a rectangle with overlap detection

---
+++ src/Core/Domain/ValueObjects/Rect.cs
@@ -0,0 +1,17 @@
+namespace HeuteApp.Core.Domain.ValueObjects;
+
+public readonly record struct Rect(
+    int X,
+    int Y,
+    int Width,
+    int Height)
+{
+    public bool Overlaps(Rect other)
+    {
+        return
+            X < other.X + other.Width &&
+            X + Width > other.X &&
+            Y < other.Y + other.Height &&
+            Y + Height > other.Y;
+    }
+}
