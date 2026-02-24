47d72b600b83d000c51e39346660446201528842
k4yr2 <kyrzkaya@gmail.com>
2026-02-24 15:51:31 +0300

    Add position property to HeuteBoardCard for grid placement

--- src/Core/Domain/HeuteBoardCard.cs
+++ src/Core/Domain/HeuteBoardCard.cs
@@ -1,6 +1,14 @@
+using HeuteApp.Core.Domain.ValueObjects;
+
 namespace HeuteApp.Core.Domain;
 
 public class HeuteBoardCard(Guid id)
 {
+    private GridRect? m_position = null;
+
+    //
+
     public Guid Id => id;
+
+    public GridRect? Position => m_position;
 }
