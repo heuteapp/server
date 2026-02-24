f18e8a21e2f482a7e907de3230814d731405002a
k4yr2 <kyrzkaya@gmail.com>
2026-02-24 17:21:14 +0300

    Add Rect property to HeuteLayoutSection for rectangle representation

--- src/Core/Domain/HeuteLayoutSection.cs
+++ src/Core/Domain/HeuteLayoutSection.cs
@@ -1,10 +1,16 @@
+using HeuteApp.Core.Domain.ValueObjects;
+
 namespace HeuteApp.Core.Domain;
 
 public class HeuteLayoutSection(string id)
 {
     private readonly string m_id = id;
 
+    private readonly Rect? m_rect = null;
+
     //
     
     public string Id => m_id;
+
+    public Rect? Rect => m_rect;
 }
