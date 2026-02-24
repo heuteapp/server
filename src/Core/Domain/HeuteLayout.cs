02756636683853b8c34ca104a3e765ae014de022
k4yr2 <kyrzkaya@gmail.com>
2026-02-24 17:46:55 +0300

    Refactor HeuteLayout class to initialize section properties and improve section retrieval

--- src/Core/Domain/HeuteLayout.cs
+++ src/Core/Domain/HeuteLayout.cs
@@ -2,5 +2,14 @@ namespace HeuteApp.Core.Domain;
 
 public class HeuteLayout(string id)
 {
+
+    private HeuteLayoutSection[]? m_sections = null;
+
+    private readonly Dictionary<string, HeuteLayoutSection> m_sectionDictionary = [];
+
+    //
+
     public string Id => id;
+
+    public HeuteLayoutSection[] Sections => m_sections ??= [.. m_sectionDictionary.Values];
 }
