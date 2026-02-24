e4feab4bd757ad4aab53d3b354267c49810ee74a
k4yr2 <kyrzkaya@gmail.com>
2026-02-24 17:22:46 +0300

    Add Size property to HeuteLayoutSection for grid size representation

--- src/Core/Domain/HeuteLayoutSection.cs
+++ src/Core/Domain/HeuteLayoutSection.cs
@@ -8,9 +8,13 @@ public class HeuteLayoutSection(string id)
 
     private readonly Rect? m_rect = null;
 
+    private readonly GridSize? m_size = null;
+
     //
     
     public string Id => m_id;
 
     public Rect? Rect => m_rect;
+
+    public GridSize? Size => m_size;
 }
