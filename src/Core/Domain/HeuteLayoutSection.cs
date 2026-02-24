0f73f3bca984008da6bec3aa905e3cd58adf355d
k4yr2 <kyrzkaya@gmail.com>
2026-02-24 17:30:52 +0300

    Refactor HeuteLayoutSection to use constructor parameter directly for Id property

--- src/Core/Domain/HeuteLayoutSection.cs
+++ src/Core/Domain/HeuteLayoutSection.cs
@@ -4,15 +4,13 @@ namespace HeuteApp.Core.Domain;
 
 public class HeuteLayoutSection(string id)
 {
-    private readonly string m_id = id;
-
     private readonly Rect? m_rect = null;
 
     private readonly GridSize? m_size = null;
 
     //
     
-    public string Id => m_id;
+    public string Id => id;
 
     public Rect? Rect => m_rect;
 
