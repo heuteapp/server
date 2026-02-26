42d1561d49f3a90c3040a1fd285abcfdbfe17095
k4yr2 <kyrzkaya@gmail.com>
2026-02-26 10:46:32 +0300

    Remove UpdateAsync method from IBoardRepository interface

--- src/Core/Repositories/IBoardRepository.cs
+++ src/Core/Repositories/IBoardRepository.cs
@@ -7,6 +7,4 @@ public interface IBoardRepository
     Task<HeuteBoard?> GetByDateAsync(Guid ownerId, DateOnly date);
 
     Task AddAsync(HeuteBoard board);
-
-    Task UpdateAsync(HeuteBoard board);
 }
