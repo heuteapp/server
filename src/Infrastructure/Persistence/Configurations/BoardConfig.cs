50ff8d6bb74cbfe2cd3d8677b963554821de6b0b
k4yr2 <kyrzkaya@gmail.com>
2026-02-26 10:55:12 +0300

    Add BoardConfig class for BoardEntity configuration in Entity Framework

---
+++ src/Infrastructure/Persistence/Configurations/BoardConfig.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using HeuteApp.Infrastructure.Persistence.Entities;
+
+namespace HeuteApp.Infrastructure.Persistence.Configurations;
+
+public class BoardConfig : IEntityTypeConfiguration<BoardEntity>
+{
+    public void Configure(EntityTypeBuilder<BoardEntity> builder)
+    {
+        builder.ToTable("boards");
+
+        builder.HasKey(b => b.Id);
+
+        builder.Property(b => b.OwnerId)
+            .IsRequired();
+
+        builder.Property(b => b.LayoutId)
+            .IsRequired();
+
+        builder.HasMany(b => b.Cards)
+            .WithOne(c => c.Board)
+            .HasForeignKey(c => c.BoardId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne<LayoutEntity>()
+            .WithMany()
+            .HasForeignKey(b => b.LayoutId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
