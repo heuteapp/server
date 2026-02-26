5328a0668a668e75ba8c19a309013f140eddd115
k4yr2 <kyrzkaya@gmail.com>
2026-02-26 11:13:11 +0300

    Add LayoutSectionConfig class for LayoutSectionEntity configuration in Entity Framework

---
+++ src/Infrastructure/Persistence/Configurations/LayoutSectionConfig.cs
@@ -0,0 +1,60 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using HeuteApp.Infrastructure.Persistence.Entities;
+
+namespace HeuteApp.Infrastructure.Persistence.Configurations;
+
+public class LayoutSectionConfig : IEntityTypeConfiguration<LayoutSectionEntity>
+{
+    public void Configure(EntityTypeBuilder<LayoutSectionEntity> builder)
+    {
+        builder.ToTable("layout_sections");
+
+        builder.HasKey(s => s.Id);
+
+        builder.Property(s => s.LayoutId)
+               .IsRequired();
+
+        builder.Property(s => s.Name)
+               .IsRequired();
+
+        builder.HasIndex(s => s.LayoutId);
+
+        // Rect Value Object
+        builder.OwnsOne(s => s.Rect, rect =>
+        {
+            rect.Property(r => r.X)
+                .HasColumnName("rect_x")
+                .IsRequired();
+
+            rect.Property(r => r.Y)
+                .HasColumnName("rect_y")
+                .IsRequired();
+
+            rect.Property(r => r.Width)
+                .HasColumnName("rect_width")
+                .IsRequired();
+
+            rect.Property(r => r.Height)
+                .HasColumnName("rect_height")
+                .IsRequired();
+        });
+
+        builder.Navigation(s => s.Rect)
+               .IsRequired();
+
+        builder.OwnsOne(s => s.Size, size =>
+        {
+            size.Property(sz => sz.ColCount)
+                .HasColumnName("size_col_count")
+                .IsRequired();
+
+            size.Property(sz => sz.RowCount)
+                .HasColumnName("size_row_count")
+                .IsRequired();
+        });
+
+        builder.Navigation(s => s.Size)
+               .IsRequired();
+    }
+}
