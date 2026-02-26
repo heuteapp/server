40f84cc200a355604565b5b1afac5601c0e349ae
k4yr2 <kyrzkaya@gmail.com>
2026-02-26 11:02:04 +0300

    Add BoardCardConfig class for BoardCardEntity configuration in Entity Framework

---
+++ src/Infrastructure/Persistence/Configurations/BoardCardConfig.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using HeuteApp.Infrastructure.Persistence.Entities;
+
+namespace HeuteApp.Infrastructure.Persistence.Configurations;
+
+public class BoardCardConfig : IEntityTypeConfiguration<BoardCardEntity>
+{
+    public void Configure(EntityTypeBuilder<BoardCardEntity> builder)
+    {
+        builder.ToTable("board_cards");
+
+        builder.HasKey(c => c.Id);
+
+        builder.Property(c => c.BoardId)
+               .IsRequired();
+
+        builder.Property(c => c.SectionId);
+
+        builder.Property(c => c.Title)
+               .IsRequired();
+
+        builder.OwnsOne(c => c.Position, position =>
+        {
+            position.Property(p => p.Col)
+                    .HasColumnName("position_col")
+                    .IsRequired();
+
+            position.Property(p => p.Row)
+                    .HasColumnName("position_row")
+                    .IsRequired();
+
+            position.Property(p => p.ColSpan)
+                    .HasColumnName("position_col_span")
+                    .IsRequired();
+
+            position.Property(p => p.RowSpan)
+                    .HasColumnName("position_row_span")
+                    .IsRequired();
+        });
+
+        builder.Navigation(c => c.Position)
+               .IsRequired(false);
+
+        builder.HasIndex(c => c.BoardId);
+    }
+}
