4258197c0354cb0639cb69274fd30da4eb5c9404
k4yr2 <kyrzkaya@gmail.com>
2026-02-26 11:14:29 +0300

    Refactor HeuteDbContext to apply entity configurations from assembly, removing explicit entity configurations

--- src/Infrastructure/Persistence/HeuteDBContext.cs
+++ src/Infrastructure/Persistence/HeuteDBContext.cs
@@ -1,6 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using HeuteApp.Infrastructure.Persistence.Entities;
-using HeuteApp.Core.ValueObjects;
 
 namespace HeuteApp.Infrastructure.Persistence;
 
@@ -16,76 +15,7 @@ public class HeuteDbContext(DbContextOptions<HeuteDbContext> options) : DbContex
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<BoardEntity>(builder =>
-        {
-            builder.ToTable("boards");
-
-            builder.HasKey(b => b.Id);
-
-            builder.Property(b => b.OwnerId).IsRequired();
-            builder.Property(b => b.LayoutId).IsRequired();
-
-            builder.HasMany(b => b.Cards)
-                .WithOne(c => c.Board)
-                .HasForeignKey(c => c.BoardId)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            builder.HasOne<LayoutEntity>()
-                .WithMany()
-                .HasForeignKey(b => b.LayoutId)
-                .OnDelete(DeleteBehavior.Restrict);
-        });
-
-        modelBuilder.Entity<BoardCardEntity>(builder =>
-        {
-            builder.ToTable("board_cards");
-
-            builder.HasKey(c => c.Id);
-
-            builder.Property(c => c.BoardId).IsRequired();
-            builder.Property(c => c.SectionId);
-            builder.Property(c => c.Title).IsRequired();
-
-            builder.HasIndex(c => c.BoardId);
-        });
-
-        modelBuilder.Entity<BoardCardEntity>()
-            .OwnsOne(x => x.Position);
-
-        modelBuilder.Entity<LayoutEntity>(builder =>
-        {
-            builder.ToTable("layouts");
-
-            builder.HasKey(l => l.Id);
-
-            builder.Property(l => l.OwnerId).IsRequired();
-            builder.Property(l => l.Name).IsRequired();
-            builder.Property(l => l.Version).IsRequired();
-
-            builder.HasMany(l => l.Sections)
-                .WithOne(s => s.Layout)
-                .HasForeignKey(s => s.LayoutId)
-                .OnDelete(DeleteBehavior.Cascade);
-
-            builder.HasIndex(l => new { l.OwnerId, l.Name, l.Version }).IsUnique();
-        });
-
-        modelBuilder.Entity<LayoutSectionEntity>(builder =>
-        {
-            builder.ToTable("layout_sections");
-
-            builder.HasKey(s => s.Id);
-
-            builder.Property(s => s.LayoutId).IsRequired();
-            builder.Property(s => s.Name).IsRequired();
-
-            builder.HasIndex(s => s.LayoutId);
-        });
-
-        modelBuilder.Entity<LayoutSectionEntity>()
-            .OwnsOne(x => x.Rect);
-
-        modelBuilder.Entity<LayoutSectionEntity>()
-            .OwnsOne(x => x.Size);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(HeuteDbContext).Assembly);
     }
+
 }
