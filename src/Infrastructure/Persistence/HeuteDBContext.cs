644404568d17c29fdb6a9b5a698a16487c343ac1
k4yr2 <kyrzkaya@gmail.com>
2026-02-26 09:28:22 +0300

    Add LayoutEntity configuration to HeuteDbContext for database mapping

--- src/Infrastructure/Persistence/HeuteDBContext.cs
+++ src/Infrastructure/Persistence/HeuteDBContext.cs
@@ -42,5 +42,21 @@ public class HeuteDbContext(DbContextOptions<HeuteDbContext> options) : DbContex
 
         modelBuilder.Entity<BoardCardEntity>()
             .OwnsOne(x => x.Position);
+
+        modelBuilder.Entity<LayoutEntity>(builder =>
+        {
+            builder.ToTable("layouts");
+
+            builder.HasKey(l => l.Id);
+
+            builder.Property(l => l.OwnerId).IsRequired();
+            builder.Property(l => l.Name).IsRequired();
+            builder.Property(l => l.Version).IsRequired();
+
+            builder.HasMany(l => l.Sections)
+                .WithOne(s => s.Layout)
+                .HasForeignKey(s => s.LayoutId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
     }
 }
