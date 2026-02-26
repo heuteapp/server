6498c50fc5cc2f8d1989b863095eb94a0a10c8b7
k4yr2 <kyrzkaya@gmail.com>
2026-02-27 01:43:37 +0300

    Add ToDomainModel method to BoardMapper for converting BoardEntity to HeuteBoard

--- src/Infrastructure/Mappers/BoardMapper.cs
+++ src/Infrastructure/Mappers/BoardMapper.cs
@@ -5,6 +5,11 @@ namespace HeuteApp.Infrastructure.Mappers;
 
 public static class BoardMapper
 {
+    public static HeuteBoard ToDomainModel(this BoardEntity entity)
+    {
+        return HeuteBoard.FromSnapshot(entity.ToSnapshot());
+    }
+
     public static HeuteBoardSnapshot ToSnapshot(this BoardEntity entity)
     {
         return new HeuteBoardSnapshot(
