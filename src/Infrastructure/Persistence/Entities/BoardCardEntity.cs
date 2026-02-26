1574490441a6246eeb0dd06f51d73b143966bcf2
k4yr2 <kyrzkaya@gmail.com>
2026-02-26 03:01:39 +0300

    Add BoardCardEntity class to represent board card data in persistence layer

---
+++ src/Infrastructure/Persistence/Entities/BoardCardEntity.cs
@@ -0,0 +1,16 @@
+using HeuteApp.Core.ValueObjects;
+
+namespace HeuteApp.Infrastructure.Persistence.Entities;
+
+public class BoardCardEntity
+{
+    public Guid Id { get; set; }
+
+    public Guid BoardId { get; set; }
+    
+    public string Title { get; set; } = string.Empty;
+
+    public Guid? SectionId { get; set; }
+
+    public GridRect? Position { get; set; }
+}
