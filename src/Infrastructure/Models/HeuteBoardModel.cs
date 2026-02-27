477e12b24aa3b710aa5037c54fa9cfe370dd0424
k4yr2 <kyrzkaya@gmail.com>
2026-02-27 15:28:39 +0300

    Add private constructor to HeuteBoardModel for improved encapsulation

--- src/Infrastructure/Models/HeuteBoardModel.cs
+++ src/Infrastructure/Models/HeuteBoardModel.cs
@@ -4,5 +4,15 @@ namespace HeuteApp.Infrastructure.Models;
 
 public class HeuteBoardModel : HeuteBoard
 {
+    private HeuteBoardModel() : base(Guid.Empty, Guid.Empty, Guid.Empty, default)
+    {
+        
+    }
+
+    public HeuteBoardModel(Guid id, Guid ownerId, Guid layoutId, DateOnly date) : base(id, ownerId, layoutId, date)
+    {
+        
+    }
+
     public List<BoardCardModel> Cards { get; set; } = [];
 }
