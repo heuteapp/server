61c6939d54f55a2fac3fff45140fd66fc2ffe810
k4yr2 <kyrzkaya@gmail.com>
2026-02-27 21:44:55 +0300

    Refactor BoardCardMapper to improve variable naming for clarity

--- src/Infrastructure/Mappers/BoardCardMapper.cs
+++ src/Infrastructure/Mappers/BoardCardMapper.cs
@@ -1,27 +1,33 @@
 using HeuteApp.Core.Entities;
 using HeuteApp.Core.Mappers;
-using HeuteApp.Infrastructure.Models;
+using HeuteApp.Infrastructure.Models.Entities;
 
 namespace HeuteApp.Infrastructure.Mappers;
 
 public static class BoardCardMapper
 {
-    public static BoardCard ToDomain(this BoardCardModel entity)
+    public static BoardCard ToDomain(this BoardCardModel model)
     {
-        return entity.ToProps().ToDomain(entity.Id);
+        return model.ToProps().ToDomain(model.Id);
     }
 
-    public static BoardCardProps ToProps(this BoardCardModel entity)
+    public static BoardCardProps ToProps(this BoardCardModel model)
     {
         return new BoardCardProps(
-            entity.Title,
-            entity.SectionId,
-            entity.Position
+            model.Title,
+            model.SectionId,
+            model.Position
         );
     }
 
     public static BoardCardModel ToModel(this BoardCard card, Guid boardId)
     {
         return new BoardCardModel()
+        {
+            BoardId = boardId,
+            Title = card.Title,
+            SectionId = card.SectionId,
+            Position = card.Position
+        };
     }
 }
