15bd277b1f58965bdcc2ef4f9729711eac5e6aa9
k4yr2 <kyrzkaya@gmail.com>
2026-02-27 13:11:54 +0300

    Refactor BoardMapper to remove snapshot methods and update ToProps implementations

--- src/Core/Mappers/BoardMapper.cs
+++ src/Core/Mappers/BoardMapper.cs
@@ -10,7 +10,7 @@ public static partial class BoardMapper
         ArgumentNullException.ThrowIfNull(board);
 
         return new HeuteBoardProps(
-            [.. board.Cards.Select(c => c.ToSnapshot())]
+            [.. board.Cards]
         );
     }
 
@@ -25,50 +25,13 @@ public static partial class BoardMapper
         );
     }
 
-    public static HeuteBoardSnapshot ToSnapshot(this HeuteBoard board)
-    {
-        ArgumentNullException.ThrowIfNull(board);
-
-        return new HeuteBoardSnapshot(
-            board.Id,
-            board.OwnerId,
-            board.LayoutId,
-            board.Date,
-            board.ToProps()
-        );
-    }
-
-    public static BoardCardSnapshot ToSnapshot(this BoardCard card)
-    {
-        ArgumentNullException.ThrowIfNull(card);
-
-        return new BoardCardSnapshot(
-            card.Id,
-            card.ToProps()
-        );
-    }
-
     //
-
-    public static HeuteBoard ToDomain(this HeuteBoardSnapshot snapshot)
-    {
-        ArgumentNullException.ThrowIfNull(snapshot);
-
-        return HeuteBoard.Create(snapshot.Id, snapshot.OwnerId, snapshot.LayoutId, snapshot.Date, snapshot.Props);
-    }
 
     public static HeuteBoard ToDomain(this HeuteBoardProps props, Guid id, Guid ownerId, Guid layoutId, DateOnly date)
     {
         ArgumentNullException.ThrowIfNull(props);
 
         return HeuteBoard.Create(id, ownerId, layoutId, date, props);
-    }
-
-    public static BoardCard ToDomain(this BoardCardSnapshot snapshot)
-    {
-        ArgumentNullException.ThrowIfNull(snapshot);
-
-        return BoardCard.Create(snapshot.Id, snapshot.Props);
     }
 
     public static BoardCard ToDomain(this BoardCardProps props, Guid id)
