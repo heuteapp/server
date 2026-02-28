221de3d735320109c5fe0d597bbfb797532402b8
k4yr2 <kyrzkaya@gmail.com>
2026-02-28 12:46:40 +0300

    Refactor LayoutConfig to improve code formatting and maintain consistency

--- src/Infrastructure/Configurations/LayoutConfig.cs
+++ src/Infrastructure/Configurations/LayoutConfig.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using HeuteApp.Infrastructure.Models.Aggregates;
+using HeuteApp.Infrastructure.Models.Entities;
 
 namespace HeuteApp.Infrastructure.Configurations;
 
@@ -8,25 +9,28 @@ public class LayoutConfig : IEntityTypeConfiguration<HeuteLayoutModel>
 {
     public void Configure(EntityTypeBuilder<HeuteLayoutModel> builder)
     {
-        builder.ToTable("layouts");
+       builder.ToTable("layouts");
 
-        builder.HasKey(l => l.Id);
+       builder.HasKey(l => l.Id);
 
-        builder.Property(l => l.OwnerId)
-               .IsRequired();
+       builder.Property(l => l.OwnerId)
+              .IsRequired();
 
-        builder.Property(l => l.Name)
-               .IsRequired();
+       builder.Property(l => l.Name)
+              .IsRequired();
 
-        builder.Property(l => l.Version)
-               .IsRequired();
+       builder.Property(l => l.Version)
+              .IsRequired();
 
-        builder.HasMany(l => l.Sections)
-               .WithOne(s => s.Layout)
-               .HasForeignKey(s => s.LayoutId)
-               .OnDelete(DeleteBehavior.Cascade);
+        builder.HasMany<LayoutSectionModel>("m_sections")
+              .WithOne(s => s.Layout)
+              .HasForeignKey(s => s.LayoutId)
+              .OnDelete(DeleteBehavior.Cascade);
 
-        builder.HasIndex(l => new { l.OwnerId, l.Name, l.Version })
-               .IsUnique();
+       builder.HasIndex(l => new { l.OwnerId, l.Name, l.Version })
+              .IsUnique();
+
+       builder.Navigation("m_sections")
+            .UsePropertyAccessMode(PropertyAccessMode.Field);
     }
 }
