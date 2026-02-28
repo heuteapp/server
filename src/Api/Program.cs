8cdccb7fda182bd6e77d4c0dac5cd466923516ea
k4yr2 <kyrzkaya@gmail.com>
2026-02-28 14:07:48 +0300

    Add BoardService to dependency injection in Program.cs

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using HeuteApp.Infrastructure.Persistence;
+using HeuteApp.Application.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,7 @@ builder.Services.AddDbContext<HeuteDbContext>(options =>
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<BoardService, BoardService>();
 
 var app = builder.Build();
 
