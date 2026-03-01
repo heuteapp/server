5ed68a28fe1735979fc271c77fba9a7c689d8565
k4yr2 <kyrzkaya@gmail.com>
2026-03-01 04:36:16 +0300

    Refactor Program.cs to improve database connection setup and enable dynamic JSON support

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -3,20 +3,27 @@ using HeuteApp.Infrastructure.Persistence;
 using HeuteApp.Application.Services;
 using HeuteApp.Application.Interfaces;
 using HeuteApp.Infrastructure.Repositories;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+var dataSourceBuilder = new NpgsqlDataSourceBuilder(
+    builder.Configuration.GetConnectionString("DefaultConnection"));
+
+dataSourceBuilder.EnableDynamicJson();
+
+var dataSource = dataSourceBuilder.Build();
 
 builder.Services.AddDbContext<HeuteDbContext>(options =>
 {
-    options.UseNpgsql(
-        builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseNpgsql(dataSource);
 });
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IBoardRepository, BoardRepository>();
 builder.Services.AddScoped<ILayoutRepository, LayoutRepository>();
