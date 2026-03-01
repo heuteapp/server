a9902c759ced4101f92908fdcc494348ea09418f
k4yr2 <kyrzkaya@gmail.com>
2026-03-01 14:20:39 +0300

    Fix formatting in LayoutRequestModel by adding missing newlines and ensuring proper structure

--- src/Api/Models/Request/LayoutResuestModel.cs
+++ src/Api/Models/Request/LayoutResuestModel.cs
@@ -3,3 +3,5 @@ namespace HeuteApp.Api.Models.Request;
 public record GetLayoutRequest(Guid OwnerId, string Name, int Version);
 
 public record GetLayoutsRequest(Guid OwnerId);
+
+public record CreateLayoutRequest(Guid OwnerId, string Name, int Version);
