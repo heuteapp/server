ca16ada426eecf51fa52072f936f86a3f3dad076
k4yr2 <kyrzkaya@gmail.com>
2026-03-01 11:29:55 +0300

    Add GetBoardByDateAsync method to BoardService for retrieving boards by date

--- src/Application/Services/BoardService.cs
+++ src/Application/Services/BoardService.cs
@@ -6,6 +6,11 @@ namespace HeuteApp.Application.Services;
 
 public class BoardService(IBoardRepository boardRepository, ILayoutRepository layoutRepository, IUnitOfWork unitOfWork)
 {
+    public async Task<HeuteBoard?> GetBoardByDateAsync(Guid ownerId, DateOnly date)
+    {
+        return await boardRepository.GetByDateAsync(ownerId, date);
+    }
+
     public async Task CreateBoardAsync(Guid ownerId, HeuteLayout layout, DateOnly date)
     {
         var existing = await boardRepository
