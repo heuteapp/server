f520fe1703f8352c9d798ba9129649795ae820ab
k4yr2 <kyrzkaya@gmail.com>
2026-03-02 20:21:19 +0300

    Add LayoutKey record to represent layout key structure

--- src/Core/ValueObjects/LayoutKey.cs
+++ src/Core/ValueObjects/LayoutKey.cs
@@ -2,7 +2,7 @@ using HeuteApp.Core.Enums;
 
 namespace HeuteApp.Core.ValueObjects;
 
-public sealed record LayoutRef(
+public sealed record LayoutKey(
     LayoutScopeType Scope,
     string Name,
     int Version
