a3a8ea8ce16fc2508cb5810ee665523f6b09aae0
k4yr2 <kyrzkaya@gmail.com>
2026-03-02 08:55:47 +0300

    Add LayoutResponseMapper to convert layout results to public response models

---
+++ src/Api/Mappers/Layout/LayoutResponseMapper.cs
@@ -0,0 +1,31 @@
+using HeuteApp.Api.Models.Public.Responses.Layout;
+using HeuteApp.Application.Results.Layout;
+
+namespace HeuteApp.Api.Mappers.Layout;
+
+public static class LayoutResponseMapper
+{
+    public static PublicHeuteLayoutResponse ToPublicResponse(
+        this HeuteLayoutResult result)
+    {
+        return new PublicHeuteLayoutResponse(
+            result.Name,
+            result.Version,
+            [.. result.Sections.Select(s => s.ToPublicResponse())]
+        );
+    }
+
+    public static PublicLayoutSectionResponse ToPublicResponse(
+        this LayoutSectionResult section)
+    {
+        return new PublicLayoutSectionResponse(
+            section.Name,
+            section.X,
+            section.Y,
+            section.Width,
+            section.Height,
+            section.ColCount,
+            section.RowCount
+        );
+    }
+}
