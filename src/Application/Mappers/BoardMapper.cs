225fe867f6af0105bac5a1def7a0f900c3182172
k4yr2 <kyrzkaya@gmail.com>
2026-03-02 09:24:25 +0300

    Add BoardMapper to convert board and card entities to result models

---
+++ src/Application/Mappers/BoardMapper.cs
@@ -0,0 +1,35 @@
+using HeuteApp.Application.Results.Board;
+using HeuteApp.Core.Aggregates.Board;
+
+namespace HeuteApp.Application.Mappers;
+
+public static class BoardMapper
+{
+    public static HeuteBoardResult ToResult(this HeuteBoard board)
+    {
+        ArgumentNullException.ThrowIfNull(board);
+
+        return new HeuteBoardResult(
+            board.Id,
+            board.OwnerId,
+            board.LayoutId,
+            board.Date,
+            [..board.Cards.Select(ToResult)]
+        );
+    }
+
+    public static BoardCardResult ToResult(this BoardCard card)
+    {
+        ArgumentNullException.ThrowIfNull(card);
+
+        return new BoardCardResult(
+            card.Id,
+            card.Title,
+            card.SectionId,
+            card.Position?.Col ?? 0,
+            card.Position?.Row ?? 0,
+            card.Position?.ColSpan ?? 0,
+            card.Position?.RowSpan ?? 0
+        );
+    }
+}
