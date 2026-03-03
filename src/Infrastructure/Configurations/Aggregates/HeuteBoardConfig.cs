bdf101e3527668449ddb44b390807f8319c63d4b
k4yr2 <kyrzkaya@gmail.com>
2026-03-03 16:43:47 +0300

    Refactor BoardConfig to use property access for Cards navigation and remove unused navigation configuration

--- src/Infrastructure/Configurations/Aggregates/HeuteBoardConfig.cs
+++ src/Infrastructure/Configurations/Aggregates/HeuteBoardConfig.cs
@@ -24,7 +24,7 @@ public class BoardConfig : IEntityTypeConfiguration<HeuteBoardModel>
         builder.Property(b => b.Date)
             .IsRequired();
 
-        builder.HasMany<BoardCardModel>("m_cards")
+        builder.HasMany(b => (IEnumerable<BoardCardModel>)b.Cards)
             .WithOne(c => c.Board)
             .HasForeignKey(c => c.BoardId)
             .OnDelete(DeleteBehavior.Cascade);
@@ -34,7 +34,9 @@ public class BoardConfig : IEntityTypeConfiguration<HeuteBoardModel>
             .HasForeignKey(b => b.LayoutId)
             .OnDelete(DeleteBehavior.Restrict);
 
-        builder.Navigation("m_cards")
-            .UsePropertyAccessMode(PropertyAccessMode.Field);
+        builder.HasIndex(b => b.Id)
+            .IsUnique();
+
+        builder.HasIndex(b => new { b.OwnerId, b.Date });
     }
 }
