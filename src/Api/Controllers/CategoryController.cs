64c7327921cc60b740d4feb0205c9fa247f1801f
k4yr2 <kyrzkaya@gmail.com>
2026-03-04 10:06:23 +0300

    Add CategoryController for managing categories

---
+++ src/Api/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+using HeuteApp.Api.Models.Public.Request;
+using HeuteApp.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HeuteApp.Api.Controllers;
+
+[ApiController]
+[Route("user/{ownerName}/categories")]
+public class CategoryController(CategoryService categoryService) : ControllerBase
+{
+    [HttpGet("{name}")]
+    public async Task<IActionResult> GetCategory(string ownerName, string name)
+    {
+        var category = await categoryService.GetCategoryByKeyAsync(ownerName, new (name));
+
+        if(category == null)
+            return NotFound("Category not found for the given name.");
+
+        return Ok(category);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateCategory(string ownerName, [FromBody] CreateCategoryRequest request)
+    {
+        var category = await categoryService.CreateCategoryAsync(ownerName, request.Key, request.Props);
+
+        return Ok(category);
+    }
+}
