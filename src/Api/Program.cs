7f95943dd4a995e4d7a6aea4b223ff679f515888
k4yr2 <kyrzkaya@gmail.com>
2026-03-11 14:00:22 +0300

    Refactor Program.cs to streamline service configuration and middleware setup

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,77 +1,29 @@
-using Microsoft.EntityFrameworkCore;
-using HeuteApp.Infrastructure.Persistence;
-using HeuteApp.Application.Services;
-using HeuteApp.Application.Interfaces;
-using HeuteApp.Infrastructure.Repositories;
-using Npgsql;
-using HeuteApp.Core.Services;
-using HeuteApp.Api.Services.Singletons;
-using HeuteApp.Api.Services.Contexts;
-using HeuteApp.Api.Services.Middlewares;
+using HeuteApp.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-var dataSourceBuilder = new NpgsqlDataSourceBuilder(
-    builder.Configuration.GetConnectionString("DefaultConnection"));
+builder.AddServices();
+builder.AddCors();
 
-dataSourceBuilder.EnableDynamicJson();
-
-var dataSource = dataSourceBuilder.Build();
-
-builder.Services.AddDbContext<HeuteDbContext>(options =>
+if(builder.Environment.IsDevelopment())
 {
-    options.UseNpgsql(dataSource);
-});
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddSingleton<SupabaseProvider>();
-
-builder.Services.AddScoped<HttpClient>();
-
-
-builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-builder.Services.AddScoped<IProfileRepository, ProfileRepository>();
-builder.Services.AddScoped<ILayoutRepository, LayoutRepository>();
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<IBoardRepository, BoardRepository>();
-builder.Services.AddScoped<BoardPlacementService>();
-
-builder.Services.AddScoped<ProfileService>();
-builder.Services.AddScoped<LayoutService>();
-builder.Services.AddScoped<CategoryService>();
-builder.Services.AddScoped<BoardService>();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend",
-        policy =>
-        {
-            policy
-                .WithOrigins("http://localhost:3000")
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-        });
-});
-
-
-var app = builder.Build();
-app.UseCors("AllowFrontend");
-
-builder.Services.AddScoped<UserContext>();
-app.UseMiddleware<JwtMiddleware>();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.AddSwagger();
 }
 
+var app = builder.Build();
+
 app.UseHttpsRedirection();
+app.UseAppCors();
+app.UseAppMiddlewares();
 app.UseAuthorization();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseAppSwagger();
+}
+
 app.MapControllers();
 
 app.Run();
