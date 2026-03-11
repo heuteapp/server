62c0d0af767cb5bf64cf98c6f01d7a25529a8dde
k4yr2 <kyrzkaya@gmail.com>
2026-03-12 00:56:56 +0300

    Add migration to rename 'Title' column to 'Content_Title' in 'board_cards' table

---
+++ src/Infrastructure/Migrations/20260311215439_InitialCreate2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HeuteApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "board_cards",
+                newName: "Content_Title");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Content_Title",
+                table: "board_cards",
+                newName: "Title");
+        }
+    }
+}
