ba5a8cfcdab974fb5cafb578da0fc88777477a78
k4yr2 <kyrzkaya@gmail.com>
2026-03-12 17:42:48 +0300

    Refactor BoardCardContent to use property syntax and provide a constructor for title initialization

--- src/Core/ValueObjects/Board/BoardCardContent.cs
+++ src/Core/ValueObjects/Board/BoardCardContent.cs
@@ -1,5 +1,15 @@
 namespace HeuteApp.Core.ValueObjects.Board;
 
-public record BoardCardContent(
-    string? Title
-);
+public record BoardCardContent
+{
+    public string Title { get; init; } = null!;
+
+    //
+
+    private BoardCardContent() { }
+
+    public BoardCardContent(string title)
+    {
+        Title = title;
+    }
+}
