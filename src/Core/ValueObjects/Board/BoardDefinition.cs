d2b70a7d02c16ec70599ef2593e332b4b9ce2bb5
k4yr2 <kyrzkaya@gmail.com>
2026-03-12 13:48:04 +0300

    Add constructor to BoardDefinition for initializing with Date and Cards

--- src/Core/ValueObjects/Board/BoardDefinition.cs
+++ src/Core/ValueObjects/Board/BoardDefinition.cs
@@ -3,12 +3,22 @@ namespace HeuteApp.Core.ValueObjects.Board;
 public record BoardDefinition
 {
     public BoardDefinition(
+        DateOnly Date,
+        IReadOnlyCollection<BoardCardDefinition> Cards)
+    {
+        this.Date = Date;
+        this.Cards = Cards;
+    }
+
+    public BoardDefinition(
         BoardKey Key,
         BoardProps Props)
     {
         Date = Key.Date;
         Cards = Props.Cards;
     }
+
+    //
 
     public DateOnly Date { get; }
 
