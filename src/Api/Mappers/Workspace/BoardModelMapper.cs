524a5d62b9c024c3b486991d092c1ed9612cd4ca
k4yr2 <kyrzkaya@gmail.com>
2026-03-13 01:39:59 +0300

    Add BoardModelMapper for converting HeuteBoard to BoardResponse

---
+++ src/Api/Mappers/Workspace/BoardModelMapper.cs
@@ -0,0 +1,21 @@
+using HeuteApp.Api.Models.Responses.Workspace.Board;
+using HeuteApp.Core.Aggregates.Board;
+
+namespace HeuteApp.Api.Mappers.Workspace;
+
+public static class BoardModelMapper
+{
+    public static BoardResponse ToResponse(this HeuteBoard board)
+    {
+        var cards = board.Cards.Select(card => new BoardCardResponse(
+            Title: card.Content.Title,
+            SectionName: card.Placement?.SectionName,
+            ColIndex: card.Placement?.ColIndex,
+            RowIndex: card.Placement?.RowIndex,
+            ColSpan: card.Placement?.ColSpan,
+            RowSpan: card.Placement?.RowSpan
+        ));
+
+        return new BoardResponse(cards);
+    }
+}
