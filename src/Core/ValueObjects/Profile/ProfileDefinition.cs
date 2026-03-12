bf6d4b5b7bc75977c1505583672f5355434ef01e
k4yr2 <kyrzkaya@gmail.com>
2026-03-12 18:53:48 +0300

    Refactor ProfileDefinition and ProfileProps to use property syntax, add static Empty properties, and initialize fields with default values

--- src/Core/ValueObjects/Profile/ProfileDefinition.cs
+++ src/Core/ValueObjects/Profile/ProfileDefinition.cs
@@ -1,6 +1,41 @@
 namespace HeuteApp.Core.ValueObjects.Profile;
 
-public sealed record ProfileDefinition(
-    Guid Id,
-    ProfileProps Props
-);
+public sealed record ProfileDefinition
+{
+    public static ProfileDefinition Empty => new();
+
+    //
+
+    public Guid Id { get; private set; } = Guid.Empty;
+
+    public string Name { get; private set; } = string.Empty;
+
+    public string Version { get; private set; } = string.Empty;
+
+    //
+
+    private ProfileDefinition() { }
+
+    public ProfileDefinition(
+        Guid id, 
+        string name, 
+        string version)
+    {
+        Id = id;
+        Name = name;
+        Version = version;
+    }
+
+    public ProfileDefinition(
+        Guid id,
+        ProfileProps props)
+    {
+        Id = id;
+        Name = props.Name;
+        Version = props.Email;
+    }
+
+    //
+
+    public ProfileProps Props => new(Name, Version);
+}
--- src/Core/ValueObjects/Profile/ProfileProps.cs
+++ src/Core/ValueObjects/Profile/ProfileProps.cs
@@ -1,6 +1,24 @@
 namespace HeuteApp.Core.ValueObjects.Profile;
 
-public sealed record ProfileProps(
-    string Name,
-    string Email
-);
+public sealed record ProfileProps
+{
+    public static ProfileProps Empty => new();
+
+    //
+
+    public string Name { get; private set; } = string.Empty;
+
+    public string Email { get; private set; } = string.Empty;
+
+    //
+
+    private ProfileProps() { }
+
+    public ProfileProps(
+        string name, 
+        string email)
+    {
+        Name = name;
+        Email = email;
+    }
+}
