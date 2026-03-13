3b2a2d20dc5bbc8be4b3b47be4f8cf1b20603a55
k4yr2 <kyrzkaya@gmail.com>
2026-03-13 20:54:54 +0300

    Refactor SupabaseProvider to initialize Client with configuration and environment variables for improved flexibility

--- src/Api/Services/Singletons/SupabaseProvider.cs
+++ src/Api/Services/Singletons/SupabaseProvider.cs
@@ -1,9 +1,17 @@
 namespace HeuteApp.Api.Services.Singletons;
 
-public sealed class SupabaseProvider(IConfiguration configuration)
+public sealed class SupabaseProvider
 {
-    public Supabase.Client Client { get; } = new Supabase.Client(
-        configuration["Supabase:Url"]!,
-        configuration["Supabase:ServiceKey"]!
-    );
+    public Supabase.Client Client { get; }
+
+    public SupabaseProvider(IConfiguration configuration)
+    {
+        string url = Environment.GetEnvironmentVariable("SUPABASE_URL") 
+        ?? configuration["Supabase:Url"]!;
+
+        string key = Environment.GetEnvironmentVariable("SUPABASE_KEY") 
+        ?? configuration["Supabase:Key"]!;
+
+        Client = new Supabase.Client(url, key);
+    }
 }
