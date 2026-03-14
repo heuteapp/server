1c0734a176122a3ed336941de9c9f297bf7d3b3b
k4yr2 <kyrzkaya@gmail.com>
2026-03-14 03:54:39 +0300

    Add HandleCreateCardPayload and HandleDeleteCardPayload methods to BoardCommandPayloadsMapper

--- src/Core/Mappers/Commands/Payloads/BoardPayloadsMapper.cs
+++ src/Core/Mappers/Commands/Payloads/BoardPayloadsMapper.cs
@@ -5,6 +5,23 @@ namespace HeuteApp.Core.Mappers.Commands.Payloads;
 
 public static class BoardCommandPayloadsMapper
 {
+    public static CreateCardPayload HandleCreateCardPayload(JsonElement payload)
+    {
+        var definitionElement = payload.GetProperty("definition");
+
+        return new CreateCardPayload(
+            Definition: new(
+                definitionElement.GetProperty("name").GetString()!,
+                definitionElement.GetProperty("title").GetString(),
+                definitionElement.GetProperty("sectionName").GetString(),
+                definitionElement.GetProperty("colIndex").GetInt32(),
+                definitionElement.GetProperty("rowIndex").GetInt32(),
+                definitionElement.GetProperty("rowIndex").GetInt32(),
+                definitionElement.GetProperty("rowIndex").GetInt32()
+            )
+        );
+    }
+
     public static PlaceCardPayload HandlePlaceCardPayload(JsonElement payload)
     {
         var keyElement = payload.GetProperty("key");
@@ -20,6 +37,17 @@ public static class BoardCommandPayloadsMapper
                 placementElement.GetProperty("rowIndex").GetInt32(),
                 placementElement.GetProperty("rowIndex").GetInt32(),
                 placementElement.GetProperty("rowIndex").GetInt32()
+            )
+        );
+    }
+
+    public static DeleteCardPayload HandleDeleteCardPayload(JsonElement payload)
+    {
+        var keyElement = payload.GetProperty("key");
+
+        return new DeleteCardPayload(
+            Key: new(
+                keyElement.GetProperty("name").GetString()!
             )
         );
     }
