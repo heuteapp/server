a96b7761cf4aa01b4011072dee97d8f895eb2a09
k4yr2 <kyrzkaya@gmail.com>
2026-03-16 22:16:28 +0300

    Remove IUserContext parameter from ExecuteAsync method in UserBasedActionService

--- src/Application/Services/UserBased/UserBasedActionService.cs
+++ src/Application/Services/UserBased/UserBasedActionService.cs
@@ -3,13 +3,14 @@ using HeuteApp.Application.Interfaces.UserBased;
 namespace HeuteApp.Application.Services.UserBased;
 
 public class UserBasedActionService(
+    IUserContext userContext, 
     IUserBasedActionContextFactory userBasedActionContextFactory,
     UserBasedCommandService userBasedCommandService,
     CategoryService categoryService,
     BoardService boardService,
     LayoutService layoutService)
 {
-    public async Task<TResult> ExecuteAsync<TResult>(IUserContext userContext, Func<IUserBasedActionContext, Task<TResult>> func)
+    public async Task<TResult> ExecuteAsync<TResult>(Func<IUserBasedActionContext, Task<TResult>> func)
     {
         if(!userContext.UserId.HasValue){
             throw new UnauthorizedAccessException("Unauthorized: No user context found.");
