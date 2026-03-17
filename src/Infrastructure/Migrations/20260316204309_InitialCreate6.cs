1b72b250cc99a4f83e998b22456a21fe67013ed4
k4yr2 <kyrzkaya@gmail.com>
2026-03-17 10:40:32 +0300

    Rename "Name" column to "Username" in profiles table for consistency

---
+++ src/Infrastructure/Migrations/20260316204309_InitialCreate6.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HeuteApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate6 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "profiles",
+                newName: "Username");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "profiles",
+                newName: "Name");
+        }
+    }
+}
