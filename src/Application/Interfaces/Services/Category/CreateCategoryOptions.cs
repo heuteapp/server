8441834fa72c6a8f27eceaa5023c2c6bc1e0a155
k4yr2 <kyrzkaya@gmail.com>
2026-03-29 02:45:30 +0300

    feat: add missing using directive for ParentNotFoundBehavior in CreateCategoryOptions

--- src/Application/Interfaces/Services/Category/CreateCategoryOptions.cs
+++ src/Application/Interfaces/Services/Category/CreateCategoryOptions.cs
@@ -1,6 +1,8 @@
+using HeuteApp.Application.Enums.Services;
+
 namespace HeuteApp.Application.Interfaces.Services.Category;
 
 public sealed class CreateCategoryOptions : CreateConflictOptions
 {
-    
+    public ParentNotFoundBehavior ParentNotFoundBehavior { get; init; } = ParentNotFoundBehavior.Throw;
 }
