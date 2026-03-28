6e0f1292169a7af3b020e74c5e114dcef8f02cf2
k4yr2 <kyrzkaya@gmail.com>
2026-03-29 01:47:08 +0300

    feat: add CategoryCreateStatus enum to represent various category creation statuses

---
+++ src/Application/Enums/Results/Category/Repository/CategoryCreateStatus.cs
@@ -0,0 +1,12 @@
+namespace HeuteApp.Application.Enums.Results.Category.Repository;
+
+public enum CategoryCreateStatus
+{
+    Success,
+
+    AlreadyExists,
+
+    InvalidParent,
+
+    InvalidOwner
+}
