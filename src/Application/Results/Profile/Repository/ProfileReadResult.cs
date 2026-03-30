1b30ec144da25eb1eb1d44e06107c48538ba6c65
k4yr2 <kyrzkaya@gmail.com>
2026-03-30 06:34:22 +0300

    feat: remove unused ProfileCreateResult and ProfileReadResult classes

--- src/Application/Results/Profile/Repository/ProfileReadResult.cs
+++
@@ -1,9 +0,0 @@
-using HeuteApp.Application.Results.Repository;
-using HeuteApp.Core.Aggregates.Profile;
-
-namespace HeuteApp.Application.Results.Profile.Repository;
-
-public record ProfileReadResult : ReadResult<HeuteProfile>
-{
-    
-}
