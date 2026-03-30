9338bc79c5740059c2c516ecb4265e962dca9006
k4yr2 <kyrzkaya@gmail.com>
2026-03-31 00:03:41 +0300

    feat: refactor LayoutController to use PublicLayoutService for layout retrieval

--- src/Api/Controllers/LayoutController.cs
+++ src/Api/Controllers/LayoutController.cs
@@ -1,4 +1,5 @@
 using HeuteApp.Api.Mappers.Workspace;
+using HeuteApp.Application.Services.Public;
 using HeuteApp.Application.Services.UserBased;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,18 +8,34 @@ namespace HeuteApp.Api.Controllers;
 [ApiController]
 [Route("layout")]
 public class LayoutController(
-    UserBasedLayoutService layoutService
+    PublicLayoutService publicLayoutService,
+    UserBasedActionService userBasedActionService
 ) : ControllerBase
 {
-    [HttpGet("{name}")]
+    [HttpGet("global/{name}")]
     public async Task<IActionResult> GetLayout(string name, [FromQuery] int? version)
     {
-        var layout = await layoutService.GetLayoutAsync(name, version);
+        var layout = await publicLayoutService.GetLayoutByNameAsync(name, version);
 
         if(layout == null){
             return NotFound($"Layout with name '{name}' and version '{version}' not found for the current user.");
         }
 
         return Ok(layout.ToResponse());
+    }
+
+    [HttpGet("user/{name}")]
+    public async Task<IActionResult> GetUserLayout(string name, [FromQuery] int? version)
+    {
+        return await userBasedActionService.ExecuteAsync<IActionResult>(async context =>
+        {
+            var layout = await publicLayoutService.GetLayoutByNameAsync(name, version);
+
+            if(layout == null){
+                return NotFound($"Layout with name '{name}' and version '{version}' not found for the current user.");
+            }
+
+            return Ok(layout.ToResponse());
+        });
     }
 }
