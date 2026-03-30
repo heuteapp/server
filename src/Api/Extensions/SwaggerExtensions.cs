e3b0b92529a9a2c1bc4d9a3c96c849c7c743f231
k4yr2 <kyrzkaya@gmail.com>
2026-03-30 21:47:08 +0300

    feat: enhance Swagger setup with security definitions and improved documentation options

--- src/Api/Extensions/SwaggerExtensions.cs
+++ src/Api/Extensions/SwaggerExtensions.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 namespace HeuteApp.Api.Extensions;
 
 public static class SwaggerExtensions
@@ -5,12 +7,51 @@ public static class SwaggerExtensions
     public static void AddSwagger(this WebApplicationBuilder builder)
     {
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "HeuteApp API",
+                Version = "v1",
+                Description = "HeuteApp Workspace API"
+            });
+
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description = "JWT Authorization header using the Bearer scheme.\n\n" +
+                              "Enter your token below.\n\n" +
+                              "Example: \"eyJhbGciOiJIUzI1NiIs...\""
+            });
+
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
     }
 
     public static void UseAppSwagger(this WebApplication app)
     {
         app.UseSwagger();
-        app.UseSwaggerUI();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "HeuteApp API v1");
+            options.DocumentTitle = "HeuteApp API Documentation";
+        });
     }
 }
