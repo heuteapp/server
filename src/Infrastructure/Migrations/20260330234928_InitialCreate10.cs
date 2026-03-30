946030eb3acd2646d864dec69e642ed004d47aa5
k4yr2 <kyrzkaya@gmail.com>
2026-03-31 02:49:51 +0300

    refactor: update unique index on dailyboards to include CategoryId

---
+++ src/Infrastructure/Migrations/20260330234928_InitialCreate10.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HeuteApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate10 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_dailyboards_UserId_Date",
+                table: "dailyboards");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_dailyboards_UserId_CategoryId_Date",
+                table: "dailyboards",
+                columns: new[] { "UserId", "CategoryId", "Date" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_dailyboards_UserId_CategoryId_Date",
+                table: "dailyboards");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_dailyboards_UserId_Date",
+                table: "dailyboards",
+                columns: new[] { "UserId", "Date" },
+                unique: true);
+        }
+    }
+}
