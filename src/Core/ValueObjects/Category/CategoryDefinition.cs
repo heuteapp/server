0b2bfe962b077bd5a9add427d6b822377a20c0fb
k4yr2 <kyrzkaya@gmail.com>
2026-03-31 02:43:39 +0300

    refactor: simplify Empty property initialization and enhance constructors in CategoryDefinition and CategoryKey value objects

--- src/Core/ValueObjects/Category/CategoryDefinition.cs
+++ src/Core/ValueObjects/Category/CategoryDefinition.cs
@@ -1,15 +1,21 @@
+using System.Text.Json.Serialization;
+
 namespace HeuteApp.Core.ValueObjects.Category;
 
 public sealed record CategoryDefinition(string Name)
 {
-    public static CategoryDefinition Empty => new("");
+    public static CategoryDefinition Empty => new();
 
     //
+
+    public CategoryDefinition() 
+        : this(string.Empty) { }
 
     public CategoryDefinition(CategoryKey key)
         : this(key.Name) { }
 
     //
 
+    [JsonIgnore]
     public CategoryKey Key => new(Name);
 }
--- src/Core/ValueObjects/Category/CategoryKey.cs
+++ src/Core/ValueObjects/Category/CategoryKey.cs
@@ -3,4 +3,7 @@ namespace HeuteApp.Core.ValueObjects.Category;
 public sealed record CategoryKey(string Name)
 {
     public static CategoryKey Empty => new("");
+
+    public CategoryKey() 
+        : this(string.Empty) { }
 }
