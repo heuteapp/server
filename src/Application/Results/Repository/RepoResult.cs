3606509b533a72793745413ebded48aae37e8254
k4yr2 <kyrzkaya@gmail.com>
2026-03-30 05:36:06 +0300

    feat: add RepoResult abstract record for standardized result handling

---
+++ src/Application/Results/Repository/RepoResult.cs
@@ -0,0 +1,16 @@
+namespace HeuteApp.Application.Results.Repository;
+
+public abstract record RepoResult
+{
+    public bool IsSuccess { get; init; }
+    public string? ErrorMessage { get; init; }
+    
+    protected static T Failure<T>(string errorMessage) where T : RepoResult, new()
+    {
+        return new T
+        {
+            IsSuccess = false,
+            ErrorMessage = errorMessage
+        };
+    }
+}
