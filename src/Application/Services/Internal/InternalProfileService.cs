9b9b5b03af155aa2a04b4248ddbb83bd105ef0ab
k4yr2 <kyrzkaya@gmail.com>
2026-03-30 07:19:47 +0300

    feat: update GetProfileByIdAsync to return ProfileResult and map entity to result

--- src/Application/Services/Internal/InternalProfileService.cs
+++ src/Application/Services/Internal/InternalProfileService.cs
@@ -1,16 +1,17 @@
 using HeuteApp.Application.Interfaces.Repositories;
-using HeuteApp.Core.Aggregates.Profile;
+using HeuteApp.Application.Mappers;
+using HeuteApp.Application.Results.Profile;
 
 namespace HeuteApp.Application.Services.Internal;
 
 public class InternalProfileService(
     IProfileRepository repository)
 {
-    public async Task<HeuteProfile> GetProfileByIdAsync(Guid profileId)
+    public async Task<ProfileResult> GetProfileByIdAsync(Guid profileId)
     {
         var result = await repository.ReadByIdAsync(profileId);
         result.ThrowIfFailure($"Failed to retrieve profile with ID {profileId}");
 
-        return result.Entity!;
+        return result.Entity!.ToResult();
     }
 }
