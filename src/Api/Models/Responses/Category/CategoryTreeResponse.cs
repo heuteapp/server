08750ac18244372d17c73808a4ee23c592de9a40
k4yr2 <kyrzkaya@gmail.com>
2026-03-31 22:01:10 +0300

    feat: create CategoryHierarchyResponse model for category tree structure

--- src/Api/Models/Responses/Category/CategoryTreeResponse.cs
+++ src/Api/Models/Responses/Category/CategoryTreeResponse.cs
@@ -4,6 +4,3 @@ public record CategoryTreeResponse(
     string Name,
     IEnumerable<CategoryTreeResponse>? Children
 );
-
-public record CategoryRootTreeResponse(string Name = "/", IEnumerable<CategoryTreeResponse>? Children = null) 
-    : CategoryTreeResponse(Name, Children);
