8641897766c72d55ca86486f5986bd3383775266
k4yr2 <kyrzkaya@gmail.com>
2026-03-31 16:13:39 +0300

    feat: add CategoryRootTreeResponse for root category structure

--- src/Api/Models/Responses/Category/CategoryTreeResponse.cs
+++ src/Api/Models/Responses/Category/CategoryTreeResponse.cs
@@ -4,3 +4,6 @@ public record CategoryTreeResponse(
     string Name,
     IEnumerable<CategoryTreeResponse>? Children
 );
+
+public record CategoryRootTreeResponse(string Name = "/", IEnumerable<CategoryTreeResponse>? Children = null) 
+    : CategoryTreeResponse(Name, Children);
